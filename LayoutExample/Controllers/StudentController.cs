ec583d0e7b80adff7ec0042fe084c881f66b4bd5
Nieze Ben Mansour <nieze.benmansour@outlook.fr>
2024-08-29 10:50:49 +0100

    Add project files.

---
+++ LayoutExample/Controllers/StudentController.cs
@@ -0,0 +1,36 @@
+﻿using LayoutExample.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutExample.Controllers;
+
+public class StudentController : Controller
+{
+    private StudentContext _context;
+
+    public StudentController(StudentContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        return View(_context.Students.ToList());
+    }
+
+    public IActionResult Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var student = _context.Students
+            .SingleOrDefault(s => s.StudentId == id);
+        if (student == null)
+        {
+            return NotFound();
+        }
+
+        return View(student);
+    }
+}
